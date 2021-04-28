8b4647bca366a3a570cbe3f064f6599e4086b7b2
Alexsander Uglin <uglin76@mail.ru>
2021-04-28 22:24:32 +0300

    refactoring

--- packages/utilites.py
+++ packages/utilites.py
@@ -10,14 +10,14 @@ def generate_hash(path):
         for line in file:
             yield f"{line}MD5 hash: {md5(line.encode()).hexdigest()}\n"
 
-def get_wiki_links(path, data):
+def get_wiki_links(links_file, data):
     print('Get & save wiki links for countries.')
-    with open(path, 'w', encoding='utf-8') as links_file:
+    with open(links_file, 'w', encoding='utf-8') as links_file:
         for value in data:
             links_file.write(f'{value}\n')
 
-def get_hash(save_path, source_path):
+def get_hash(hash_file, source_path):
     print('Get & save MD5 hash.')
-    with open(save_path, 'w', encoding='utf-8') as md5_file:
+    with open(hash_file, 'w', encoding='utf-8') as md5_file:
         for hash_string in generate_hash(source_path):
             md5_file.write(f"{hash_string}\n")
