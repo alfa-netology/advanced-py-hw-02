65e92de972955cf2f38c3febcb91d5a8dd98c710
Alexsander Uglin <uglin76@mail.ru>
2021-04-28 22:05:15 +0300

    refactoring

--- packages/classes.py
+++ packages/classes.py
@@ -6,7 +6,7 @@ class WikiCountryLink:
         with open(data_file_path, encoding='utf-8') as file:
             data = json.load(file)
 
-        self.WIKI_URL = "https://en.wikipedia.org/wiki/"
+        self.wiki_root = "https://en.wikipedia.org/wiki/"
         self.countries_names_iter = iter(item.get('name').get('common') for item in data)
 
     def __iter__(self):
@@ -14,5 +14,5 @@ class WikiCountryLink:
 
     def __next__(self):
         country_name = next(self.countries_names_iter)
-        country_url = f'{self.WIKI_URL}{country_name.replace(" ", "_")}'
+        country_url = f'{self.wiki_root}{country_name.replace(" ", "_")}'
         return f"{country_name}, {country_url}"
