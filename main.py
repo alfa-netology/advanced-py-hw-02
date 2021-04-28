b5aae816f8803e3c9ca37bf2c3b8b0f44c8bea62
Alexsander Uglin <uglin76@mail.ru>
2021-04-28 22:17:27 +0300

    refactoring

--- main.py
+++ main.py
@@ -8,5 +8,5 @@ utilites.makedir('output/')
 links_file = 'output/links.txt'
 utilites.get_wiki_links(links_file, data)
 
-md5_hash_file = 'output/md5.txt'
-utilites.get_md5_hash(md5_hash_file, links_file)
+hash_file = 'output/md5.txt'
+utilites.get_hash(hash_file, links_file)
--- packages/utilites.py
+++ packages/utilites.py
@@ -1,14 +1,14 @@
 import os
-import hashlib
+from hashlib import md5
 
 def makedir(path):
     if os.path.exists(path) is False:
         os.mkdir(path)
 
-def generate_md5_hash(path):
+def generate_hash(path):
     with open(path, encoding="utf-8") as file:
         for line in file:
-            yield f"{line}MD5 hash: {hashlib.md5(line.encode()).hexdigest()}\n"
+            yield f"{line}MD5 hash: {md5(line.encode()).hexdigest()}\n"
 
 def get_wiki_links(path, data):
     print('Get & save wiki links for countries.')
@@ -16,8 +16,8 @@ def get_wiki_links(path, data):
         for value in data:
             links_file.write(f'{value}\n')
 
-def get_md5_hash(save_path, source_path):
+def get_hash(save_path, source_path):
     print('Get & save MD5 hash.')
     with open(save_path, 'w', encoding='utf-8') as md5_file:
-        for hash_string in generate_md5_hash(source_path):
+        for hash_string in generate_hash(source_path):
             md5_file.write(f"{hash_string}\n")
